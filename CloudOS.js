b21090d6f67ed40635719278c025fd094e2b64ef
Jessie A. Morris <jessie@jessieamorris.com>
2013-05-15 10:27:57 -0600

    Initial commit! :-D

---
+++ CloudOS.js
@@ -0,0 +1,269 @@
+;(function(){
+  window.CloudOS = {};
+
+	// ------------------------------------------------------------------------
+	// Personal Cloud Hostname
+	CloudOS.host = "cs.kobj.net";
+
+	CloudOS.appKey = "E3D243D4-B28B-11E2-A01C-1615FE2E5C38";
+
+	CloudOS.callbackURL = "http://forevr.us/";
+
+	CloudOS.sessionToken = "none";
+
+	// ------------------------------------------------------------------------
+	// Raise Sky Event
+	CloudOS.raiseEvent = function(eventDomain, eventType, eventAttributes, eventParameters, postFunction) {
+		var eid = Math.floor(Math.random()*9999999);
+		var esl = 'https://' + CloudOS.host + '/sky/event/' +
+			CloudOS.sessionToken + '/' +  eid + '/' +
+			eventDomain + '/' + eventType +
+			'?_rids=a169x727&' + eventParameters;
+
+		$.ajax({
+				type: 'POST',
+				url: esl,
+				data: eventAttributes,
+				dataType: 'json',
+				headers: {'Kobj-Session' : CloudOS.sessionToken},
+				success: postFunction,
+		})
+	};
+
+	// ------------------------------------------------------------------------
+	// Call Sky Cloud
+
+	CloudOS.skyCloud = function(Module, FuncName, callParmaters, getSuccess) {
+		var esl = 'https://' + CloudOS.host + '/sky/cloud/' +
+					Module + '/' + FuncName + '?' + callParmaters;
+
+		$.ajax({
+				type: 'GET',
+				url: esl,
+				dataType: 'json',
+				headers: {'Kobj-Session' : CloudOS.sessionToken},
+				success: getSuccess,
+//				error: function(e) {
+//						$('#modalSpinner').hide();
+//						console.log(e.message);
+//				}
+		})
+	};
+
+	// ------------------------------------------------------------------------
+	CloudOS.createChannel = function(postFunction) {
+		CloudOS.raiseEvent('cloudos', 'api_Create_Channel', { }, "", postFunction);
+	};
+
+	// ------------------------------------------------------------------------
+	CloudOS.destroyChannel = function(myToken, postFunction) {
+		CloudOS.raiseEvent('cloudos', 'api_Destroy_Channel',
+			{ "token" : myToken }, "", postFunction);
+	};
+
+	// ========================================================================
+	// Profile Management
+
+	CloudOS.getMyProfile  = function(getSuccess) {
+		CloudOS.skyCloud("pds", "get_all_me", "", getSuccess);
+	};
+
+	CloudOS.updateMyProfile  = function(eventAttributes, postFunction) {
+		var eventParameters = "element=profileUpdate.post";
+		CloudOS.raiseEvent('web', 'submit', eventAttributes, eventParameters, postFunction);
+	};
+
+	CloudOS.getFriendProfile  = function(friendToken, getSuccess) {
+		var callParmaters = "myToken=" + friendToken;
+		CloudOS.skyCloud("a169x727", "getFriendProfile", callParmaters, getSuccess);
+	};
+
+	// ========================================================================
+	// PDS Management
+
+	// ------------------------------------------------------------------------
+	CloudOS.PDSAdd  = function(namespace, pdsKey, pdsValue, postFunction) {
+		var eventAttributes = {
+			"namespace" : namespace,
+			"pdsKey"    : pdsKey,
+			"pdsValue"  : JSON.stringify(pdsValue)
+		};
+
+		CloudOS.raiseEvent('cloudos', 'api_pds_add', eventAttributes, "", postFunction);
+	};
+
+	// ------------------------------------------------------------------------
+	CloudOS.PDSDelete  = function(namespace, pdsKey, postFunction) {
+		var eventAttributes = {
+			"namespace" : namespace,
+			"pdsKey"    : pdsKey
+		};
+
+		CloudOS.raiseEvent('cloudos', 'api_pds_delete', eventAttributes, "", postFunction);
+	};
+
+	// ------------------------------------------------------------------------
+	CloudOS.PDSUpdate  = function() {
+	};
+
+	// ------------------------------------------------------------------------
+	CloudOS.PDSList  = function(namespace, getSuccess) {
+		var callParmeters = "namespace=" + namespace;
+		CloudOS.skyCloud("pds", "get_items", callParmeters, getSuccess);
+	};
+
+	// ------------------------------------------------------------------------
+	CloudOS.sendEmail  = function(ename, email, subject, body, postFunction) {
+		var eventAttributes = {
+			"ename"   : ename,
+			"email"   : email,
+			"subject" : subject,
+			"body"    : body
+		};
+		CloudOS.raiseEvent('cloudos', 'api_send_email', eventAttributes, "", postFunction);
+	};
+
+  // ------------------------------------------------------------------------
+  CloudOS.sendNotification = function(application, subject, body, priority, token, postFunction) {
+		var eventAttributes = {
+			"application" : application,
+			"subject"     : subject,
+			"body"        : body,
+			"priority"    : priority,
+			"token"       : token
+		};
+		CloudOS.raiseEvent('cloudos', 'api_send_notification', eventAttributes, "", postFunction);
+	};
+
+	// ========================================================================
+	// Subscription Management
+
+	// ------------------------------------------------------------------------
+	CloudOS.subscribe  = function(namespace, name, relationship, token, subAttributes, postFunction) {
+		var eventAttributes = {
+			"namespace"     : namespace,
+			"channelName"   : name,
+			"relationship"  : relationship,
+			"targetChannel" : token,
+			"subAttrs"      : subAttributes
+		};
+		CloudOS.raiseEvent('cloudos', 'api_subscribe', eventAttributes, "", postFunction);
+	};
+
+	// ------------------------------------------------------------------------
+	CloudOS.subscriptionList  = function(callParmeters, getSuccess) {
+		CloudOS.skyCloud("cloudos", "subscriptionList", callParmeters, getSuccess);
+	};
+
+	// ========================================================================
+	// OAuth functions
+
+	// ------------------------------------------------------------------------
+	CloudOS.getOAuthURL = function(fragment) {
+		var client_state = Math.floor(Math.random()*9999999);
+		var url = 'https://' + CloudOS.host +
+			'/oauth/authorize?response_type=code' +
+			'&redirect_uri=' + encodeURIComponent(CloudOS.callbackURL + fragment) +
+			'&client_id=' + CloudOS.appKey +
+			'&state=' + client_state;
+
+		return(url)
+	};
+
+	// ------------------------------------------------------------------------
+	CloudOS.getOAuthAccessToken  = function(code, callback) {
+    if(typeof(callback) !== 'function'){
+      callback = function(){};
+    }
+		var url = 'https://' + CloudOS.host +	'/oauth/access_token';
+		var data = {
+				"grant_type"   : "authorization_code",
+				"redirect_uri" : CloudOS.callbackURL,
+				"client_id"    : CloudOS.appKey,
+				"code"         : code
+		};
+
+		$.ajax({
+			type: 'POST',
+				url: url,
+				data: data,
+				dataType: 'json',
+				success: function(json) {
+					CloudOS.saveSession(json.OAUTH_ECI);
+          callback(json);
+				},
+			})
+	}
+
+	// ========================================================================
+	// Session Management
+
+	// ------------------------------------------------------------------------
+	CloudOS.retrieveSession  = function() {
+		var SessionCookie = kookie_retrieve();
+
+		if (SessionCookie != "undefined") {
+			CloudOS.sessionToken = SessionCookie;
+		} else {
+			CloudOS.sessionToken = "none";
+		}
+	};
+
+	// ------------------------------------------------------------------------
+	CloudOS.saveSession  = function(Session_ECI) {
+		CloudOS.sessionToken = Session_ECI;
+		kookie_create(Session_ECI);
+	};
+
+	// ------------------------------------------------------------------------
+	CloudOS.removeSession  = function() {
+		CloudOS.sessionToken = "none";
+		kookie_delete();
+	};
+
+	// ------------------------------------------------------------------------
+	CloudOS.authenticatedSession  = function() {
+		return(CloudOS.sessionToken !== "none")
+	};
+
+	var SkyTokenName = '__SkySessionToken';
+	var SkyTokenExpire = 7;
+
+	// --------------------------------------------
+	function kookie_create(SkySessionToken) {
+    if (SkyTokenExpire) {
+      var date = new Date();
+      date.setTime(date.getTime()+(SkyTokenExpire*24*60*60*1000));
+      var expires = "; expires="+date.toGMTString();
+    }
+    else var expires = "";
+    var kookie = SkyTokenName+"="+SkySessionToken+expires+"; path=/";
+    document.cookie = kookie;
+    // console.debug('(create): ', kookie);
+	}
+
+	// --------------------------------------------
+	function kookie_delete() {
+    var kookie = SkyTokenName+"=foo; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/";
+    document.cookie = kookie;
+    // console.debug('(destroy): ', kookie);
+	}
+
+	// --------------------------------------------
+	function kookie_retrieve() {
+    var TokenValue = 'undefined';
+		var TokenName  = '__SkySessionToken';
+    var allKookies = document.cookie.split('; ');
+    for (var i=0;i<allKookies.length;i++) {
+      var kookiePair = allKookies[i].split('=');
+			// console.debug("Kookie Name: ", kookiePair[0]);
+			// console.debug("Token  Name: ", TokenName);
+      if (kookiePair[0] == TokenName) {
+        TokenValue = kookiePair[1];
+      };
+    }
+    // console.debug("(retrieve) TokenValue: ", TokenValue);
+		return TokenValue;
+	}
+
+})();
---
+++ README.md
@@ -0,0 +1,128 @@
+# CloudOS.js
+
+CloudOS.js is a library that interacts with CloudOS Personal Clouds. It uses the Sky Cloud API to get data and raises events to save that data.
+
+
+## Registering your Cloud Fabric App
+1. Log into your [SquareTag](https://squaretag.com/app.html). If you have already become a developer and have the KDK, go ahead and skip to step 4.
+2. Become a developer. Go to your [settings](https://squaretag.com/app.html#!/app/a169x695/show). Choose myCloud as the application and change your Cloud Type to `cloudTypeDeveloper`.
+3. Install the KDK. Open up the [appStore](https://squaretag.com/app.html#!/app/a169x669/show) and install the KDK.
+4. Run the KDK. Open up [myApps](https://squaretag.com/app.html#!/app/a169x670/show) and click on the KDK icon.
+5. Create an app. Open the app menu and select `Create App`. Enter your application's details and save the app. The required fields are similar to other OAuth protected API's.
+6. View your new app. In order to get the developer token, you'll need to click on your newly created app.
+7. Modify CloudOS.js to include your app's base callback URL and developer token.
+
+
+## CloudOS.js Methods
+
+### `CloudOS.getOAuthURL(fragment)`
+This returns a link to display to your user. This link, when clicked on, will take the user to an OAuth authorization page.
+
+It takes one optional parameter, the fragment. With the fragment, you can return back to the current page you are on. The fragment gets appended to the `callbackURL` defined in CloudOS.js.
+
+
+### `CloudOS.getOAuthAccessToken(code, callback)`
+This function fetches the OAuth access token from the Personal Cloud. The code will be provided as a parameter in the OAuth callback URL.
+
+An optional parameter, callback, can be provided. The callback should take one parameter which is the JSON response in the following format:
+
+```
+{
+ "access_token":"OAUTH_ACCESS_TOKEN",
+ "OAUTH_ECI":"USER_ECI"
+}
+```
+
+### `CloudOS.retrieveSession()`
+Gets the stored ECI out of the session cookie. This enables logins to persist between visits. It returns nothing.
+
+### `CloudOS.removeSession()`
+Deletes the cookie. This allows logging out to stop login persistence.
+
+### `CloudOS.authenticatedSession()`
+Returns the whether or not there is a valid CloudOS ECI. Useful for determining if the user is logged in or not.
+
+### `CloudOS.createChannel(callback)`
+Creates a channel and passes the returned object to your callback function. The object is in the following format:
+
+```
+{
+  "status":true,
+  "token":"YOURNEWTOKENHERE"
+}
+```
+
+`status` is a boolean which states whether it worked or not. True means that it worked and false means it failed.
+
+### `CloudOS.destroyChannel(token, callback)`
+Deletes a channel and calls your callback function after finishing.
+
+### `CloudOS.getMyProfile(callback)`
+Fetches the currently logged in user's information. It then calls your callback function with the profile information. The returned object looks like so:
+
+```
+{
+  "myProfileName":"USERSNAME",
+  "myProfilePhoto":"URLTOPROFILEPHOTO",
+  "myProfileNotes":"PROFILENOTES",
+  "myProfileDescription":"PROFILEDESCRIPTION",
+  "myProfilePhone":"USERSPHONENUMBER",
+  "myProfileEmail":"USERSEMAIL"
+}
+```
+
+The format and fields in the profile may change.
+
+### `CloudOS.updateMyProfile(updatedProfile, callback)`
+Changes the profile and calls your callback function when finished. The updatedProfile variable should have the same options as what is returned from `CloudOS.getMyProfile`.
+
+### `CloudOS.getFriendProfile(token, callback)`
+Gets a friend's profile. In order to get a person's information, there must be a subscription set up
+between the two clouds. The returned object is the same as from `CloudOS.getMyProfile`.
+
+### `CloudOS.PDSAdd(namespace, key, value, callback)`
+Stores arbitrary data in the user's personal data store. The namespace and key are path identifiers and the value is what you wish to store in the PDS. Value can be an integer, float, boolean, array, hash, or string. Your callback function gets called once the addition is completed.
+
+Imagine you have a variable named `PDS` that is a hash. The namespace and key are used as follows:
+`PDS[namespace][key]`. The namespace and key can be any valid hash key identifiers.
+
+### `CloudOS.PDSDelete(namespace, key, callback)`
+Deletes an item from the PDS identified whose path is identified by the namespace and key. See `CloudOS.PDSAdd` for more PDS related information.
+
+
+### `CloudOS.PDSUpdate()`
+Not yet implemented.
+
+### `CloudOS.PDSList(namespace, callback)`
+Lists all of the variables stored in the PDS with the path of namespace. This includes all of the keys added using `CloudOS.PDSAdd`. Your callback should take one option that is the returned data from the PDS. The format is as follows:
+
+```
+{
+  "key1": "value1",
+  "key2": [
+    10,
+    20
+  ]
+}
+```
+
+### `CloudOS.sendEmail(name, email, subject, body, callback)`
+Sends an email to `email` with the other attributes. Calls your callback function once the email has been sent.
+
+### `CloudOS.sendNotification(applicationName, subject, body, priority, token, callback)`
+Sends a notification to the user identified by `token`. Similar to `CloudOS.sendEmail` but sends a notification to the user based on their notification settings.
+
+### `CloudOS.subscribe(namespace, name, relationship, token, subscriptionAttributes, callback)`
+Creates a subscription between the current user and the user identified by `token`. `namespace`, `name` and `relationship` are all strings identifying the type of subscription. `subscriptionAttributes' is a hash containing data you wish to include with the subscription.
+
+### `CloudOS.subscriptionList(callParameters, callback)`
+Returns all subscriptions. I have no clue what `callParameters` is, though. None what-so-ever. `callback` is the callback function we call with the subscriptions. It should accept one parameter that is the subscriptions. It looks like:
+
+TODO: fill this part out
+```
+{
+}
+```
+
+## Raising Events
+### 
